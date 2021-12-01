ac848778b2a608c01630d3aedb093802f731b3c8
Dmitry Mass <dimamikron41@gmail.com>
2021-12-02 01:09:10 +0300

    Second commit

---
+++ main.js
@@ -0,0 +1,51 @@
+// let firstNumber = parseInt(prompt("Введите первое число"));
+// let secondNumber = parseInt(prompt("Введите второе число"));
+// let symbol = prompt("Введите знак (+ - * /");
+
+// let result ;
+
+
+// if (symbol == "+") {
+//     result = firstNumber + secondNumber
+//     alert(`${firstNumber} + ${secondNumber} = ${result} `)
+    // } 
+    // else if (symbol == "-") {
+    //     result = firstNumber - secondNumber
+    //     alert(`${firstNumber} - ${secondNumber} = ${result} `)
+    //     } 
+    //     else if (symbol == "*") {
+    //         result = firstNumber * secondNumber
+    //         alert(`${firstNumber} * ${secondNumber} = ${result} `)
+    //         } 
+    //     else if (symbol == "/") {
+    //         result = firstNumber / secondNumber
+    //         alert(`${firstNumber} / ${secondNumber} = {result} `)
+    //          } 
+
+
+let firstNumber = parseInt(prompt("Введите первое число"));
+let secondNumber = parseInt(prompt("Введите второе число"));
+let symbol = prompt("Введите знак (+ - * /");
+
+let result ;
+
+function addResult (symbol) {
+    if (symbol == "+") {
+        result = firstNumber + secondNumber
+        alert(`${firstNumber} + ${secondNumber} = ${result} `)
+        } 
+        else if (symbol == "-") {
+            result = firstNumber - secondNumber
+            alert(`${firstNumber} - ${secondNumber} = ${result} `)
+            } 
+            else if (symbol == "*") {
+                result = firstNumber * secondNumber
+                alert(`${firstNumber} * ${secondNumber} = ${result} `)
+                } 
+            else if (symbol == "/") {
+                result = firstNumber / secondNumber
+                alert(`${firstNumber} / ${secondNumber} = {result} `)
+                 } 
+} 
+
+addResult(symbol);
