d929fa27bd9807cae89092c4738422c5da9b97c9
Dmitry Mass <dimamikron41@gmail.com>
2021-12-10 12:32:11 +0300

    debgs

--- main.js
+++ main.js
@@ -4,53 +4,43 @@
 
 // let result ;
 
-
 // if (symbol == "+") {
 //     result = firstNumber + secondNumber
 //     alert(`${firstNumber} + ${secondNumber} = ${result} `)
-    // } 
-    // else if (symbol == "-") {
-    //     result = firstNumber - secondNumber
-    //     alert(`${firstNumber} - ${secondNumber} = ${result} `)
-    //     } 
-    //     else if (symbol == "*") {
-    //         result = firstNumber * secondNumber
-    //         alert(`${firstNumber} * ${secondNumber} = ${result} `)
-    //         } 
-    //     else if (symbol == "/") {
-    //         result = firstNumber / secondNumber
-    //         alert(`${firstNumber} / ${secondNumber} = {result} `)
-    //          } 
-
+// }
+// else if (symbol == "-") {
+//     result = firstNumber - secondNumber
+//     alert(`${firstNumber} - ${secondNumber} = ${result} `)
+//     }
+//     else if (symbol == "*") {
+//         result = firstNumber * secondNumber
+//         alert(`${firstNumber} * ${secondNumber} = ${result} `)
+//         }
+//     else if (symbol == "/") {
+//         result = firstNumber / secondNumber
+//         alert(`${firstNumber} / ${secondNumber} = {result} `)
+//          }
 
 let firstNumber = parseInt(prompt("Введите первое число"));
 let secondNumber = parseInt(prompt("Введите второе число"));
 let symbol = prompt("Введите знак (+ - * /");
 
-let result ;
+let result;
 
-function addResult (symbol) {
-    if (symbol == "+") {
-        result = firstNumber + secondNumber
-        alert(`${firstNumber} + ${secondNumber} = ${result} `)
-        } 
-        else if (symbol == "-") {
-            result = firstNumber - secondNumber
-            alert(`${firstNumber} - ${secondNumber} = ${result} `)
-            } 
-            else if (symbol == "*") {
-                result = firstNumber * secondNumber
-                alert(`${firstNumber} * ${secondNumber} = ${result} `)
-                } 
-            else if (symbol == "/") {
-                result = firstNumber / secondNumber
-                alert(`${firstNumber} / ${secondNumber} = {result} `)
-                 } 
-} 
+function addResult(symbol) {
+  if (symbol == "+") {
+    result = firstNumber + secondNumber;
+    alert(`${firstNumber} + ${secondNumber} = ${result} `);
+  } else if (symbol == "-") {
+    result = firstNumber - secondNumber;
+    alert(`${firstNumber} - ${secondNumber} = ${result} `);
+  } else if (symbol == "*") {
+    result = firstNumber * secondNumber;
+    alert(`${firstNumber} * ${secondNumber} = ${result} `);
+  } else if (symbol == "/") {
+    result = firstNumber / secondNumber;
+    alert(`${firstNumber} / ${secondNumber} = ${result} `);
+  }
+}
 
 addResult(symbol);
-
-
-
-
-
